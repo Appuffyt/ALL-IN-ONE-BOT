8981e3f58ce8b2f985898b7fa1a7a6c0e6c9c4fa
Appuffyt <lisaexe6@gmail.com>
2025-03-15 16:58:46 +0530

    Update setserverconfig.js

--- commands/setups/setserverconfig.js
+++ commands/setups/setserverconfig.js
@@ -63,7 +63,7 @@ module.exports = {
         .setAuthor({ 
             name: "Alert!", 
             iconURL: cmdIcons.dotIcon ,
-            url: "https://discord.gg/xQF9f9yUEM"
+            url: "https://discord.gg/CJ8Y58FH"
         })
         .setDescription('- This command can only be used through slash command!\n- Please use `/setserverconfig`')
         .setTimestamp();
