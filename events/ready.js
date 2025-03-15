30fcfac81b1ed8ba33b77cf6203cde633ff01e2b
Appuffyt <lisaexe6@gmail.com>
2025-03-15 16:09:11 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,10 +6,8 @@ module.exports = {
     execute(client) {
   
         const activities = [
-            { name: 'Netflix', type: ActivityType.Watching },
-            { name: 'GTA VI', type: ActivityType.Playing },
-            { name: 'on YouTube', type: ActivityType.Streaming },
-            { name: 'Spotify', type: ActivityType.Listening },
+            { name: 'on YouTube', type: ActivityType.Streaming },  
+            url: 'https://www.youtube.com/watch?v=xDJ0JkI4jzk'
         ];
 
      
