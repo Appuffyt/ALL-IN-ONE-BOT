5432a14555d7c4613bfd1519bfe456ec1d75586a
Appuffyt <lisaexe6@gmail.com>
2025-03-15 16:16:48 +0530

    Update music.js

--- events/music.js
+++ events/music.js
@@ -107,7 +107,7 @@ module.exports = (client) => {
                 .setAuthor({
                     name: "Queue is Empty",
                     iconURL: musicIcons.alertIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/CJ8Y58FH"
                 })
                 .setDescription('**Leaving voice channel!**')
                 .setFooter({ text: 'Let the Beat Drop!', iconURL: musicIcons.footerIcon })
